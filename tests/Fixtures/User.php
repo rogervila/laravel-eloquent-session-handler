305816db51d3110841cdb92da25790483772463b
Roger Vilà <rogervila@me.com>
2022-02-23 14:10:41 +0100

    improve user relationship

--- tests/Fixtures/User.php
+++ tests/Fixtures/User.php
@@ -3,8 +3,18 @@
 namespace Tests\EloquentSessionHandler\Fixtures;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     protected $guarded = [];
+
+    public static function fake(array $fields = []): self
+    {
+        return self::create(array_merge([
+            'name' => 'foo',
+            'email' => 'foo@bar.com',
+            'password' => Hash::make('secret'),
+        ], $fields));
+    }
 }
